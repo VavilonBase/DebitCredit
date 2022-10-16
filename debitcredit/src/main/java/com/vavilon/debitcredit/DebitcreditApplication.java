5657d67f96910814f898d926ba0d370adbe939ec
VavilonBase <VavilonBase@mail.ru>
2022-10-16 11:37:37 +0300

    Initialize project

---
+++ debitcredit/src/main/java/com/vavilon/debitcredit/DebitcreditApplication.java
@@ -0,0 +1,13 @@
+package com.vavilon.debitcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitcreditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebitcreditApplication.class, args);
+    }
+
+}
---
+++ debitcredit/src/test/java/com/vavilon/debitcredit/DebitcreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vavilon.debitcredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitcreditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
